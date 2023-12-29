b1018897cbdf3c4d34a9821e1c0f58e0e798e15d
galih <samudragalih94@gmail.com>
2023-12-29 20:31:09 +0700

    feat(UI): use `ignoresSafeArea` to make `ZStack` reach whole screen

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                 .foregroundStyle(.green)
                 .font(.largeTitle)
         }
-        .background(.blue)
+        .ignoresSafeArea()
     }
 }
 
