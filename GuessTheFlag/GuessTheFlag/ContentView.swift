559673000664cdc652abab5187106db95efdafcd
galih <samudragalih94@gmail.com>
2023-12-29 21:11:25 +0700

    feat(UI): create custom `Button` with `Text` as title

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,29 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Button("Button 0") { print("Button 0 tapped") }
-                .buttonStyle(.bordered)
-                .tint(.indigo)
-            Button("Button 1") { print("Button 1 tapped") }
-                .buttonStyle(.bordered)
-            Button("Button 1.5", role: .destructive) { print("Button 1.5 tapped") }
-                .buttonStyle(.bordered)
-            Button("Button 2") { print("Button 2 tapped") }
-                .buttonStyle(.plain)
-            Button("Button 2.5", role: .destructive) { print("Button 2.5 tapped") }
-                .buttonStyle(.plain)
-            Button("Button 3") { print("Button 3 tapped") }
-                .buttonStyle(.borderless)
-            Button("Button 3.5", role: .destructive) { print("Button 3.5 tapped") }
-                .buttonStyle(.borderless)
-            Button("Button 4") { print("Button 4 tapped") }
-                .buttonStyle(.borderedProminent)
-            Button("Button 4.5", role: .destructive) { print("Button 4.5 tapped") }
-                .buttonStyle(.borderedProminent)
-            Button("Button 5") { print("Button 5 tapped") }
-                .buttonStyle(.automatic)
-            Button("Button 5.5", role: .destructive) { print("Button 5.5 tapped") }
-                .buttonStyle(.automatic)
+            Button {
+                print("Button tapped")
+            } label: {
+                Text("Tap me")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
+            }
+            
         }
         
     }
