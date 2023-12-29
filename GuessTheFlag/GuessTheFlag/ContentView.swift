b83c8fbd480b6f10d8a99faf05466a4231b057bc
galih <samudragalih94@gmail.com>
2023-12-29 21:04:53 +0700

    feat(UI): show button with template button style

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button("Delete", role: .destructive, action: executeButton)
+        VStack {
+            Button("Button 1") { print("Button 1 tapped") }
+                .buttonStyle(.bordered)
+            Button("Button 2") { print("Button 2 tapped") }
+                .buttonStyle(.plain)
+            Button("Button 3") { print("Button 3 tapped") }
+                .buttonStyle(.borderless)
+            Button("Button 4") { print("Button 4 tapped") }
+                .buttonStyle(.borderedProminent)
+            Button("Button 5") { print("Button 5 tapped") }
+                .buttonStyle(.automatic)
+        }
+        
     }
     
     private func executeButton() {
