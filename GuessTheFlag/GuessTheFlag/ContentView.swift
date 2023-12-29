d521c8ac0f4246f1e6e46388146464d95d40ed1f
galih <samudragalih94@gmail.com>
2023-12-29 20:23:22 +0700

    feat(UI): create Indonesia Flag using `ZStack`, `VStack` and `Text`

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,9 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.red
-                .frame(minWidth: 100, maxWidth: .infinity, maxHeight: 200)
-            Text("Your content")
+            Color.green
+            VStack(spacing: 0) {
+                Color.red
+                    .frame(minWidth: 100, maxWidth: .infinity, maxHeight: 100)
+                Color.white
+                    .frame(minWidth: 100, maxWidth: .infinity, maxHeight: 100)
+            }
+            Text("Indonesia")
+                .foregroundStyle(.green)
+                .font(.largeTitle)
         }
     }
 }
