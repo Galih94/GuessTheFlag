8121710c59671b3ef07e007e9edf7bc8ea50dd80
galih <samudragalih94@gmail.com>
2023-12-29 21:19:14 +0700

    feat(UI): customize `Image(systemName: "pencil")` with `foregroundStyle` and `font`

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Image(systemName: "pencil")
+            .foregroundStyle(.green)
+            .font(.largeTitle)
         
     }
     
